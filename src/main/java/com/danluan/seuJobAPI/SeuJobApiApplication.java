4edf120d733870991d74c333ed88f238dfaffd85
danluan <danielluanlourencol@gmail.com>
2024-05-04 23:57:27 -0300

    first commit

---
+++ src/main/java/com/danluan/seuJobAPI/SeuJobApiApplication.java
@@ -0,0 +1,13 @@
+package com.danluan.seuJobAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuJobApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeuJobApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danluan/seuJobAPI/SeuJobApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danluan.seuJobAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeuJobApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
