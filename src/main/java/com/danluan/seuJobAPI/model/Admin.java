278cbccffb38c5374fdbee555a6c290888b1f3b8
danluan <danielluanlourencol@gmail.com>
2024-05-06 17:18:43 -0300

    feat: admin user

---
+++ src/main/java/com/danluan/seuJobAPI/model/Admin.java
@@ -0,0 +1,39 @@
+package com.danluan.seuJobAPI.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    public Admin() {
+    }
+
+    public Admin(User user) {
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
