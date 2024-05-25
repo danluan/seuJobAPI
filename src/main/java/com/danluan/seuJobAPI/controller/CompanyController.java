4707b52838ad09f4d7d763bb2d345556e160c74b
bren1n <brenoleite1234@gmail.com>
2024-05-24 22:32:55 -0300

    fix: retorna status corretos nas requisições de Company

--- src/main/java/com/danluan/seuJobAPI/controller/CompanyController.java
+++ src/main/java/com/danluan/seuJobAPI/controller/CompanyController.java
@@ -1,11 +1,15 @@
 package com.danluan.seuJobAPI.controller;
 
+import com.danluan.seuJobAPI.exception.UserIdAlreadyInUse;
 import com.danluan.seuJobAPI.model.dto.CompanyDTO;
 import com.danluan.seuJobAPI.model.dto.CompanyUpdateDTO;
 import com.danluan.seuJobAPI.service.CompanyService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,8 +23,15 @@ public class CompanyController {
     private CompanyService companyService;
 
     @GetMapping("/{id}")
-    public CompanyDTO getCompanyById(@PathVariable Integer id) {
-        return companyService.getCompanyById(id);
+    public ResponseEntity<CompanyDTO> getCompanyById(@PathVariable Integer id) {
+        try {
+            CompanyDTO companyDTO = companyService.getCompanyById(id);
+            return new ResponseEntity<>(companyDTO, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping
@@ -29,19 +40,40 @@ public class CompanyController {
     }
 
     @PostMapping
-    public CompanyDTO createCompany(@Valid @RequestBody CompanyDTO companyDTO) {
-        return companyService.createCompany(companyDTO);
+    public ResponseEntity<CompanyDTO> createCompany(@Valid @RequestBody CompanyDTO companyDTO) {
+        try {
+            CompanyDTO createdCompany = companyService.createCompany(companyDTO);
+            return new ResponseEntity<>(createdCompany, HttpStatus.CREATED);
+        } catch (UserIdAlreadyInUse e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PutMapping("/{id}")
-    public CompanyDTO updateCompany(@Valid @RequestBody CompanyUpdateDTO companyUpdateDTO, @PathVariable Integer id) {
-        companyUpdateDTO.setId(id);
-        return companyService.updateCompany(companyUpdateDTO);
+    public ResponseEntity<CompanyDTO> updateCompany(@Valid @RequestBody CompanyUpdateDTO companyUpdateDTO, @PathVariable Integer id) {
+        try {
+            companyUpdateDTO.setId(id);
+            CompanyDTO company = companyService.updateCompany(companyUpdateDTO);
+            return new ResponseEntity<>(company, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCompany(@PathVariable Integer id) {
-        companyService.deleteCompany(id);
+    public ResponseEntity<Void> deleteCompany(@PathVariable Integer id) {
+        try {
+            companyService.deleteCompany(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
